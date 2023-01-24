01dbb30ddb35c704f848404d7205c9ba3b0b6d8d
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-24 17:55:02 +0600

    peridot: vibrator: rebrand for peridot and build
    Change-Id: I6473d5dd31ada37ebf740dd5305c972fbdd5a57d

--- device.mk
+++ device.mk
@@ -593,10 +593,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    android.hardware.vibrator-service.peridot
 
 # Verified boot
 PRODUCT_COPY_FILES += \
