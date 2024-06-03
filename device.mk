230dcb008df4d4d49a3e701d12d188b22ffcd455
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-03 20:45:23 +0530

    peridot: Declare api level
    Change-Id: I947722d129f823441119f60b788a189572825d50

--- device.mk
+++ device.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# API
+BOARD_API_LEVEL := 34
+BOARD_SHIPPING_API_LEVEL := 34
+PRODUCT_SHIPPING_API_LEVEL := 34
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
