4db3bfa62432ab6e8322bd26d4e5a80d3dfae507
AdarshGreal <adarshgrewal@gmail.com>
2024-06-11 02:38:16 +0530

    peridot: Setup kernel args
    Change-Id: Ic4c3d3676502960ab0470cffb1f4a7aa0dd8d3b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,5 +20,41 @@ TARGET_NO_BOOTLOADER := true
 # Hardware
 BOARD_USES_QCOM_HARDWARE := true
 
+# Kernel
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_NEEDS_DTBOIMAGE := true
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := Image
+
+TARGET_KERNEL_SOURCE := kernel/xiaomi/peridot
+TARGET_KERNEL_CONFIG := \
+    gki_defconfig
+
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+BOARD_INIT_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
+
+BOARD_KERNEL_CMDLINE := \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    swinfo.fingerprint=peridot:14/V816.0.2.0.UNPINXM:user \
+    mtdoops.fingerprint=peridot:14/V816.0.2.0.UNPINXM:user
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.load_modules_parallel=true \
+    androidboot.vendor.qspa=true \
+    androidboot.hypervisor.protected_vm.supported=false \
+    androidboot.selinux=permissive
+
 # Platform
 TARGET_BOARD_PLATFORM := pineapple
+
+# Recovery
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
