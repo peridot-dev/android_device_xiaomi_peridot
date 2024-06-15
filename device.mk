1d71237d8347bf2c300a428e3f93d74827d67de0
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-15 14:21:29 +0530

    peridot: Build qti power service
    Change-Id: I6182c0cf1d34a7aaa8c06919f42da54134bb4c44

--- device.mk
+++ device.mk
@@ -276,8 +276,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2.vendor \
-    android.hardware.power-V4-ndk.vendor
+    android.hardware.power-service-qti
 
 # Radio
 PRODUCT_PACKAGES += \
