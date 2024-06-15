6d9c0eedfdc52e8a7d7e3a69e04667b651c0ba2c
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-15 18:37:19 +0530

    peridot: Switch to qti vibrator service
    Change-Id: I576cdcea42c19d9e7632606872666974de9013d8

--- device.mk
+++ device.mk
@@ -100,6 +100,13 @@ PRODUCT_PACKAGES += \
     vndservice \
     vndservicemanager
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
