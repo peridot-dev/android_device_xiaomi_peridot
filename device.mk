fa8c1786beb7f2fb5d08465a5453840e16733087
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-17 00:01:45 +0530

    peridot: Build keymaster deps from source
    Change-Id: I8ac493ea26b282ab0fe62fb07bc18ec949ac1f6b

--- device.mk
+++ device.mk
@@ -249,7 +249,9 @@ PRODUCT_PACKAGES += \
     android.hardware.security.sharedsecret-V1-ndk.vendor \
     android.se.omapi-V1-ndk.vendor \
     android.system.keystore2-V1-ndk.vendor \
-    lib_android_keymaster_keymint_utils.vendor
+    lib_android_keymaster_keymint_utils.vendor \
+    libkeymaster_messages.vendor \
+    libkeymaster_portable.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
