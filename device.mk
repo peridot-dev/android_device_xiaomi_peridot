9bf1abde0390b651b0b784789a6483371fd7d68b
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-16 23:55:56 +0530

    peridot: Build qti bluetooth interfaces from source
    Change-Id: I516c56b06d3b840713f82e142bbbd0f4b0c11b6d

--- device.mk
+++ device.mk
@@ -89,6 +89,15 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.1.vendor
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth.audio-V1-ndk.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
+PRODUCT_PACKAGES += \
+    com.dsi.ant@1.0.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
