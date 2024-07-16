4453a2a6ea06d92822f6c0d92bc9c1b332af1749
AdarshGrewal <adarshgrewal@gmail.com>
2024-07-16 20:17:13 +0530

    peridot: Build fastcharge hal
    Change-Id: I4952bfefa80777a4f077286c9e8ad998f941c2e8

--- device.mk
+++ device.mk
@@ -153,6 +153,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.xiaomi
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.common.thread \
