3e9cef061dacabfdb4c59796c4345aee904fd821
AdarshGrewal <adarshgrewal@gmail.com>
2024-07-17 00:22:40 +0530

    peridot: Build servicetracker interfaces from source
    Change-Id: I7aed0feb689cf6befd2367d4b290b1baf2bdec8d

--- device.mk
+++ device.mk
@@ -488,6 +488,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.0.vendor \
+    vendor.qti.hardware.servicetracker@1.1.vendor \
+    vendor.qti.hardware.servicetrackeraidl-V1-ndk.vendor
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
