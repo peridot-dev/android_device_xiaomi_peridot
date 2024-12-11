d09243a9d8837d631ab75b10a6cb3efb68b0ed5c
Nick Reuter <nreuter85@gmail.com>
2024-12-10 20:32:13 -0600

    peridot: Address missing libbinder symbols in libwfdnative
    _ZN7android6binder12atrace_beginEmPKc
    _ZN7android6binder10atrace_endEm
    
    Change-Id: I942d51721461dbbf9c8d0f2f32128529bb7480c3

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so')
         .remove_needed('android.hidl.base@1.0.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
