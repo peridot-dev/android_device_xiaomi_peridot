744c559cb9a35fb02a55579cab1a313d02f93132
raghavt20 <raghavt20@aospa.co>
2024-12-28 16:33:57 +0530

    peridot: Update override prop KEY names
    Support for new prop overrides was added:
    https://review.lineageos.org/c/LineageOS/android_build_soong/+/401215
    
    Change-Id: If3118562011a88246539d9c38ebc7c0f613724fc

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -20,14 +20,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 24069PC21G
 
-PRODUCT_SYSTEM_NAME := peridot_global
-PRODUCT_SYSTEM_DEVICE := peridot
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="peridot_global-user 14 UKQ1.240116.001 V816.0.5.0.UNPMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.5.0.UNPMIXM:user/release-keys
+    BuildDesc="peridot_global-user 14 UKQ1.240116.001 V816.0.5.0.UNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.5.0.UNPMIXM:user/release-keys \
+    DeviceName=peridot
+    DeviceProduct=peridot_global \
+    SystemName=peridot_global \
+    SystemDevice=peridot
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
