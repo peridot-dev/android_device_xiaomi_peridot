feaaac9ede603b0ed8279b0827572ac8c5cd1aa9
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    peridot: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- device.mk
+++ device.mk
@@ -438,9 +438,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Vendor
 $(call inherit-product, vendor/xiaomi/peridot/peridot-vendor.mk)
