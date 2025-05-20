4f7f6943c731788ebdde43cf8b5612589c12064c
AdarshGrewal <adarshgrewal@gmail.com>
2025-05-20 20:13:10 +0530

    peridot: Align extract-files with template
    Change-Id: I16a2d1fa238302656191e0f1e5a77baf9f42dfc5

--- extract-files.py
+++ extract-files.py
@@ -8,11 +8,26 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
-
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/vintf/manifest/vendor.qti.qesdsys.service.xml': blob_fixup()
@@ -52,6 +67,8 @@ module = ExtractUtilsModule(
     'peridot',
     'xiaomi',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
