9c08a249bd079622bac91840d17356110b04fc0a
raghavt20 <raghavt20@aospa.co>
2025-07-31 21:18:50 +0000

    tmp! peridot: Import apktool to tree

--- extract-files.sh
+++ extract-files.sh
@@ -92,9 +92,9 @@ function blob_fixup() {
         system/priv-app/MiuiCamera/MiuiCamera.apk)
             [ "$2" = "" ] && return 0
             tmp_dir="${EXTRACT_TMP_DIR}/MiuiCamera"
-            apktool d -q "${2}" -o "${tmp_dir}" -f
+            ./apktool d -q "${2}" -o "${tmp_dir}" -f
             grep -rl "com.miui.gallery" "${tmp_dir}" | xargs sed -i 's|"com.miui.gallery"|"com.google.android.apps.photos"|g'
-            apktool b -q "${tmp_dir}" -o "${2}"
+            ./apktool b -q "${tmp_dir}" -o "${2}"
             rm -rf "${tmp_dir}"
             split --bytes=20M -d "${2}" "${2}".part
             ;;
