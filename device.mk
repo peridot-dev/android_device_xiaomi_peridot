4f52dafe9752aa0209bae235a34fe32ab801ace5
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-17 20:18:54 +0530

    peridot: Use prebuilt android keystore permission
     * the aosp one is never than what stock uses
    
    Change-Id: Id95f99ce3683dbcd011de1d6ad3447954d4cec7f

--- device.mk
+++ device.mk
@@ -209,9 +209,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.authsecret-service.nxp
 
-PRODUCT_PACKAGES += \
-    android.hardware.hardware_keystore.xml
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
