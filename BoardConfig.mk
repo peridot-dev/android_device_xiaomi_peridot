43f7f17912dc73d95f183588c312c2aec33e5ae4
AdarshGrewal <adarshgrewal@gmail.com>
2025-09-05 13:20:53 +0530

    peridot: props: Enable useage of vendor enhanced framework
     * QCC apps needs this to run
     09-05 13:14:56.224  2907  2907 E AndroidRuntime: java.lang.RuntimeException: Unable to start receiver com.qualcomm.qti.qdma.app.BootReceiver: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean com.qualcomm.qti.qdma.app.ApplicationManager.isOkToRun()' on a null object reference
    
    Change-Id: I66d0c6ba1db27bfbab12313fa878845d2247abdf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,6 +203,7 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
 
 # Recovery
