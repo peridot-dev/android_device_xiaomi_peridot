f497ae6318c8e37e04c27784dc545e28e44924a9
AdarshGrewal <adarshgrewal@gmail.com>
2025-09-16 11:27:40 +0530

    peridot: rootdir: Checkout rootdir to LA.VENDOR.14.3.0.r1-21700-lanai.0

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -69,29 +69,9 @@ else
 fi
 
 function set_density_by_fb() {
-    project=`getprop ro.build.product`
     #put default density based on width
     if [ -z $fb_width ]; then
-        case "$project" in
-            "houji")
-            setprop vendor.display.lcd_density 480
-            ;;
-            "shennong")
-            setprop vendor.display.lcd_density 560
-            ;;
-            "manet")
-            setprop vendor.display.lcd_density 560
-            ;;
-            "ruyi")
-            setprop vendor.display.lcd_density 520
-            ;;
-            "goku")
-            setprop vendor.display.lcd_density 480
-            ;;
-            *)
-            setprop vendor.display.lcd_density 440
-            ;;
-        esac
+        setprop vendor.display.lcd_density 320
     else
         if [ $fb_width -ge 1600 ]; then
            setprop vendor.display.lcd_density 640
@@ -106,17 +86,6 @@ function set_density_by_fb() {
         else
             setprop vendor.display.lcd_density 160
         fi
-        case "$project" in
-            "houji")
-            setprop vendor.display.lcd_density 480
-            ;;
-            "ruyi")
-            setprop vendor.display.lcd_density 520
-            ;;
-            "goku")
-            setprop vendor.display.lcd_density 480
-            ;;
-        esac
     fi
 }
 
