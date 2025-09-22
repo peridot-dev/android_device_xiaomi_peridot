49351c2d8c8f00b96a41922c199f0a0930363540
AdarshGrewal <adarshgrewal@gmail.com>
2025-09-22 11:22:58 +0530

    peridot: Remove liboemcrypto linker config
     * already copied in qcom/common since https://review.lineageos.org/c/LineageOS/android_hardware_qcom-caf_common/+/438956
    
    Change-Id: Iaa6ae01559107fc2e7904dcc5d366acf62708061

--- device.mk
+++ device.mk
@@ -220,10 +220,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
-# Linker
-PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
-    $(LOCAL_PATH)/configs/linker.config.json
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
