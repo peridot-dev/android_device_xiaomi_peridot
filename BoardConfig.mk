d5dc4b00eaedb05d1be073a853d8a41028c5ea55
AdarshGrewal <adarshgrewal@gmail.com>
2025-09-25 02:02:40 +0530

    a
    Change-Id: I5306bf3ba8441b0cf902289bdee80a8508385b3d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,8 +114,7 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=a600000.dwc3 \
     androidboot.load_modules_parallel=true \
     androidboot.vendor.qspa=true \
-    androidboot.hypervisor.protected_vm.supported=false \
-    androidboot.selinux=permissive
+    androidboot.hypervisor.protected_vm.supported=false
 
 # Kernel modules
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules/modules.load.system_dlkm))
