110c8af19674841ae772e6fa5ee048fea3d79153
ᴘᴏᴡᴇʀ ᡣ𐭩 ʜᴏᴜsᴇ ツ <34471968+powerhouse1997@users.noreply.github.com>
2025-11-16 11:32:43 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -346,6 +346,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.xiaomi-multihal
 
 PRODUCT_PACKAGES += \
