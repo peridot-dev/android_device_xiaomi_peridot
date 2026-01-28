8291c7cad88ba30846b9497d79151ae5028bbf3d
AdarshGrewal <adarshgrewal@gmail.com>
2026-01-29 00:56:22 +0530

    peridot: Import hal uuid map
    Change-Id: I3a49cb2ca4271f5a3f4615036c9ef50503eb573a

--- device.mk
+++ device.mk
@@ -232,6 +232,9 @@ PRODUCT_PACKAGES += \
     android.hardware.authsecret-service.nxp
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hal_uuid_map.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_peridot.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
